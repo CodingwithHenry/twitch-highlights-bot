82976872917297868d3f13ed3656eae89442cdcd
Henry Beiker <>
2025-08-17 23:14:28 +0200

    english vids only

--- run_uploads.sh
+++ run_uploads.sh
@@ -6,9 +6,9 @@ SCRIPT_PATH="/home/henry/twitch-highlights-bot/main.py"
 source "$VENV_PATH/bin/activate"
 
 # Run first game, ignore errors
-python "$SCRIPT_PATH" --game "BATTLEFIELD 6" --amount 30 --languages en de es|| echo "BATTLEFIELD 6 failed"
+python "$SCRIPT_PATH" --game "BATTLEFIELD 6" --amount 30 --languages en || echo "BATTLEFIELD 6 failed"
 
 # Run second game, ignore errors
-python "$SCRIPT_PATH" --game "League of Legends" --amount 30 --languages en de fr es || echo "League of Legends failed"
+python "$SCRIPT_PATH" --game "League of Legends" --amount 30 --languages en de  || echo "League of Legends failed"
 
 deactivate
